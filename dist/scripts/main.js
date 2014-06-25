2fe1d881df43d715d15fcd6c434fe3ed8563d791
Taylor Smith <tsmit182@gmail.com>
2014-06-25 03:07:35 -0400

    add dist to gitignore

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,23 @@
-$('div').click(function( event ) {
-  alert( event.type ); // "click"
+$('#clickAlert').click(function( event ) {
+  alert( 'Totally got you to click me.' ); // "hey "
 });
+
+// $( "#other" ).click(function() {
+//   $( "#target" ).keydown();
+// });
+
+$( "#checkMetaKey" ).click(function( event ) {
+  $( "#display" ).text( event.metaKey );
+});
+
+
+$(document).ready(function() {
+    $('#somethingHere').fadeOut(10000);
+     $('#fader').mouseenter(function() {
+        	$(this).fadeTo('fast',.1);
+    });
+});
+
+
+$('#sel').last().addClass( "selected" );
+
